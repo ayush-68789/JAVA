f7e1832150e6a8361a15e26b14972335a67ab8b0
ayush-68789 <ayush.chaurasia_cs24@gla.ac.in>
2024-12-17 21:14:37 +0530

    Create String in Java.c

---
+++ String in Java.c
@@ -0,0 +1,17 @@
+public class Main 
+{
+    public static void main (String args[])
+    {
+        //Strings
+        String name1 = "tanu" ;
+        System.out.println(name1);         // prints the string
+        
+        System.out.println(name1.length());   // prints the length of name variable
+        
+        String name2 = "ayu" ; 
+        
+        String name3 = name1 + " and " + name2 ;     // concatenate two strings 
+        
+        System.out.println(name3) ; 
+    }
+}
