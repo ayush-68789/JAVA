0209610253e90080e64c808aa6fcd73cf5139233
ayush-68789 <ayush.chaurasia_cs24@gla.ac.in>
2024-12-17 18:36:33 +0530

    Create Sum of numbers.java

---
+++ Sum of numbers.java
@@ -0,0 +1,11 @@
+public class Main 
+{
+    public static void main (String args[])
+    {
+        int a = 15 ; 
+        int b = 48 ; 
+        int sum = a + b ;
+        System.out.print(sum) ; 
+        //System.out.print(a+b) ;   //it also prints the sum without third variable
+    }
+}
