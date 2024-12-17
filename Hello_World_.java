cdb38798598d0d47c54043a87cabe283053bab6d
ayush-68789 <ayush.chaurasia_cs24@gla.ac.in>
2024-12-17 15:32:01 +0530

    Update Hello_World_.java

--- Hello_World_.java
+++ Hello_World_.java
@@ -3,6 +3,6 @@ class Main
     public static void main(String args[])
     {
         System.out.println("Hello World"); // println used to print in next line
-        System.out.print.("Hello world") ; // prints in same line
+        System.out.print.("Hello world\n") ; // or use \n 
     }
 }
