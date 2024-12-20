d78eabc645aef17b0259b680c6896f77aecab15a
ayush-68789 <ayush.chaurasia_cs24@gla.ac.in>
2024-12-20 20:30:57 +0530

    Create Simple Interest.java

---
+++ Simple Interest.java
@@ -0,0 +1,9 @@
+class Main
+{
+    public static void main(String args[])
+    {
+        int p = 2500 , r = 200 , t = 2 , si ;
+        si = (p*r*t)/100 ;
+        System.out.println("simple interest : "+si) ;
+    }
+}
