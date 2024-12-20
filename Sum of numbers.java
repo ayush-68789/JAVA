e91af49438a3c45744384a95dbeaec72ceb56b2a
ayush-68789 <ayush.chaurasia_cs24@gla.ac.in>
2024-12-20 20:05:03 +0530

    Update Sum of numbers.java

--- Sum of numbers.java
+++ Sum of numbers.java
@@ -7,5 +7,7 @@ public class Main
         int sum = a + b ;
         System.out.print("sum of a and b :"+sum) ; 
         //System.out.print(a+b) ;   //it also prints the sum without third variable
+
+        //Sytem.out.println("sum of a andd b : "+a+b) ;         it will print sum of and b : 1548
     }
 }
