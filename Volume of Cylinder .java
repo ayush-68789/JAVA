0551e1cd29c03106de31c9c85cb308bdae80ffca
ayush-68789 <ayush.chaurasia_cs24@gla.ac.in>
2024-12-20 20:23:12 +0530

    Create Volume of Cylinder .java

---
+++ Volume of Cylinder .java
@@ -0,0 +1,9 @@
+class Main
+{
+    public static void main(String args[])
+    {
+        int r = 4 , h = 2 ; 
+        int vol = (22/7)* r * r * h ;
+        System.out.println("volume is "+vol) ;
+    }
+}
