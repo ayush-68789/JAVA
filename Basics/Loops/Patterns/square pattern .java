01e734f74b90bbf0e7ac7d88528e08fc79081876
Ayush chaurasia <ayush.chaurasia_cs24@gla.ac.in>
2024-12-25 10:01:02 +0530

    Update square pattern .java

--- Basics/Loops/Patterns/square pattern .java
+++ Basics/Loops/Patterns/square pattern .java
@@ -1,1 +1,18 @@
-
+import java.util.Scanner ;
+class Main 
+{
+    public static void main(String args[]) 
+    {
+        Scanner sc = new Scanner(System.in) ;
+        int row = sc.nextInt() ;
+        int col = sc.nextInt() ;
+        for(int i = 1 ; i <= row ; i++)
+        {
+            for(int j = 1 ; j <= col ; j++)
+            {
+                System.out.print("* ") ;
+            }
+            System.out.print("\n") ;
+        }
+    }
+}
