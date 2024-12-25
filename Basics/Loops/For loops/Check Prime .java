3caf58ef9b8eecbf678c32f6654109335cac4f4e
Ayush chaurasia <ayush.chaurasia_cs24@gla.ac.in>
2024-12-25 10:56:20 +0530

    Create Check Prime .java

---
+++ Basics/Loops/For loops/Check Prime .java
@@ -0,0 +1,28 @@
+import java.util.Scanner ;
+class Main 
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in) ;
+        System.out.print("Enter any number : ") ;
+        int num = sc.nextInt() ;
+        sc.close() ;
+        int count  = 0 ; 
+        for(int i = 1 ; i <= num ; i++)
+        {
+            if(num % i == 0)
+            {
+                count++ ;
+            }
+        }
+        
+        if(count==2)
+        {
+            System.out.println(num+" is PRIME") ;
+        }
+        else
+        {
+            System.out.println(num+" is NOT PRIME") ;
+        }
+    }
+}
