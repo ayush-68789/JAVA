398ffa8a91293ffddb178cc7fb13d14fe87f6fd2
Ayush chaurasia <ayush.chaurasia_cs24@gla.ac.in>
2024-12-26 16:09:53 +0530

    Create String Functions 1 .java

---
+++ Strings/String Functions 1 .java
@@ -0,0 +1,14 @@
+import java.util.Scanner ;
+class Main 
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in) ;
+        String name = sc.nextLine() ;
+        //System.out.println(name.toLowerCase()) ;      //print to lowercase
+        //System.out.println(name.toUpperCase()) ;      // print to uppercase
+        //System.out.println(name.trim()) ;             // print string after removing trailing and ending spaces
+        //System.out.println(name.substring(4));        // return the substring starting with give index to the end of string
+        //System.out.println(name.substring(3,8));      // return the substring with given starting and ending index o string
+    }
+}
