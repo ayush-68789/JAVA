49992d2c32eccd9072020dc26721f3470978fcaa
Ayush chaurasia <ayush.chaurasia_cs24@gla.ac.in>
2024-12-27 19:46:14 +0530

    Create String Functions 2 .java

---
+++ Strings/String Functions 2 .java
@@ -0,0 +1,18 @@
+import java.util.Scanner ; 
+class Main 
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in) ;
+        String str = sc.nextLine() ;
+        //System.out.println(str.startsWith("Ay")) ;            // return true when string starts with given words
+        //System.out.print(str.endsWith("sh")) ;                // return true when string ends with given wwords
+        //System.out.print(str.charAt(4)) ;                     // returns the character at given index
+        //System.out.print(str.indexOf("s")) ;                  // return the index of the given character
+        //System.out.print(str.indexOf("g",3)) ;                // return the index after the given index when not present prints -1
+        //System.out.print(str.lastIndexOf("s")) ;              // returns the last index of character
+        //System.out.print(str.lastIndexOf("s",2)) ;            // returns the last index of character from the index given 
+        //System.out.print(str.equals("ayush")) ;               // return true if given str is equal to the inputted str 
+        //System.out.print(str.equalsIgnoreCase("ayush")) ;     // same thing as equals but ignore the case of string
+    }
+}
