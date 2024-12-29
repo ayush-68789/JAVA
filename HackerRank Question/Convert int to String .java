634db6f00f21fdc337b5b15fc378c7fb6579638e
Ayush chaurasia <ayush.chaurasia_cs24@gla.ac.in>
2024-12-29 17:53:19 +0530

    Create Convert int to String .java

---
+++ HackerRank Question/Convert int to String .java
@@ -0,0 +1,11 @@
+import java.util.Scanner ;
+class Main 
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner (System.in) ;
+        int n = sc.nextInt() ;
+        String str = Integer.toString(n) ;
+        System.out.print(str) ; ;
+    }
+}
