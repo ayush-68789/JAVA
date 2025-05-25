2620e53025f5b4c5757391a99f3ee1735378cec6
Ayush chaurasia <ayush.chaurasia_cs24@gla.ac.in>
2025-05-25 10:42:37 +0530

    Update Factorial of number in .java

--- Basics/Loops/For loops/Factorial of number in .java
+++ Basics/Loops/For loops/Factorial of number in .java
@@ -1,17 +1,26 @@
-import java.util.Scanner ;
-class Main 
+#TERMINAL 
+enter number : 5
+Factorial : 120
+
+
+    
+#code
+    
+import java.util.Scanner;
+class Main
 {
     public static void main(String args[])
     {
         Scanner sc = new Scanner(System.in) ;
-        System.out.printf("enter a number : ") ;
-        int num = sc.nextInt() ;
+        System.out.print("enter number : ");
+        int n = sc.nextInt() ;
+        
         int fact = 1 ; 
-        for(int i = 1 ; i <= num ; i++)
+        for (int i = 1 ; i <= n ; i++)
         {
-            fact = fact * i ;
+            fact*=i;
         }
-        sc.close() ;
-        System.out.printf("Factorial of %d = %d",num,fact) ;
+        
+        System.out.print("Factorial : "+fact);
     }
 }
