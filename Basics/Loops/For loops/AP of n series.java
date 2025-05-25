ea8eeda5c5976dce6120257a8cf34a0f46964aaa
Ayush chaurasia <ayush.chaurasia_cs24@gla.ac.in>
2025-05-25 10:24:37 +0530

    Update AP of n series.java

--- Basics/Loops/For loops/AP of n series.java
+++ Basics/Loops/For loops/AP of n series.java
@@ -1,15 +1,39 @@
+# TERMINAL
+Enter series : 10
+Enter First Term : 3
+Enter diff : -2
+3
+1
+-1
+-3
+-5
+-7
+-9
+-11
+-13
+-15
+
+#code
+
 import java.util.Scanner ;
 class Main 
 {
-    public static void main(String args[])
+    public static void main(String args [])
     {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("enter number : ");
-        int n = sc.nextInt() ;
-        int sum = 0 ;
-        for(int i = 1 ; i <= 2*n-1 ; i+=2)      // last term of ap is 2n-1
+        Scanner sc = new Scanner(System.in) ;
+        System.out.print("Enter series : ");
+        int n = sc.nextInt();
+        
+        System.out.print("Enter First Term : ");
+        int a = sc.nextInt() ;
+        
+        System.out.print("Enter diff : "); 
+        int diff = sc.nextInt() ; 
+        
+        for (int i = 1 ; i <= n ; i++)     
         {
-            System.out.println(sum+i) ;
+            System.out.println(a + (i-1)*diff);   // A + (N-1)D
         }
+        sc.close()
     }
 }
