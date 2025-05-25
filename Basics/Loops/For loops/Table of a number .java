63119b2f76ff5e7f5bfea0dc41f89d21617e4b78
Ayush chaurasia <ayush.chaurasia_cs24@gla.ac.in>
2025-05-25 10:54:48 +0530

    Update Table of a number .java

--- Basics/Loops/For loops/Table of a number .java
+++ Basics/Loops/For loops/Table of a number .java
@@ -1,14 +1,32 @@
+#TERMINAL 
+Enter num : 5
+5 X 1 = 5
+5 X 2 = 10
+5 X 3 = 15
+5 X 4 = 20
+5 X 5 = 25
+5 X 6 = 30
+5 X 7 = 35
+5 X 8 = 40
+5 X 9 = 45
+5 X 10 = 50
+
+    
+#CODE 
+    
 import java.util.Scanner ;
 class Main 
 {
-    public static void main(String args[])
+    public  static void main(String args[])
     {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("enter number : ");
+        Scanner sc = new Scanner(System.in) ;
+        
+        System.out.print("Enter num : ");
         int n = sc.nextInt() ;
-        for(int i = 1 ; i <= 10 ; i++)
+        
+        for (int i = 1 ; i <= 10 ; i++)
         {
-            System.out.println(n*i) ;
+            System.out.printf("%d X %d = %d\n",n,i,n*i);
         }
     }
 }
