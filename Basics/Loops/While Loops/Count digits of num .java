c810223628cb9da574ff293163e6a8dd18b19e2f
Ayush chaurasia <ayush.chaurasia_cs24@gla.ac.in>
2025-05-25 17:16:41 +0530

    Create Count digits of num .java

---
+++ Basics/Loops/While Loops/Count digits of num .java
@@ -0,0 +1,28 @@
+#TERMINAL
+532
+10
+
+
+  #code
+import java.util.Scanner;
+class Main 
+{
+    public static void digitsum(int n)
+    {
+        int sum = 0 ;
+        while(n>0)
+        {
+            sum += n % 10 ;
+            n = n/10 ;
+        }
+        n = sum ;
+        
+        System.out.printf("%d",n);
+    }
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        digitsum(n);
+    }
+}
