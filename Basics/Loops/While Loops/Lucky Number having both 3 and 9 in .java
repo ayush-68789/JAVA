ab86adef187e8decf2feb6083c493d348aa24e7f
Ayush chaurasia <ayush.chaurasia_cs24@gla.ac.in>
2025-05-27 20:17:53 +0530

    Create Lucky Number having both 3 and 9 in .java

---
+++ Basics/Loops/While Loops/Lucky Number having both 3 and 9 in .java
@@ -0,0 +1,31 @@
+import java.util.Scanner ;
+class Main 
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt() ;
+        int three = 0 , nine = 0 ;
+        while (n>0)
+        {
+            if (n%10 == 3)
+            {
+                three = 1; 
+            }
+            else if (n%10 == 9)
+            {
+                nine = 1 ;
+            }
+            n= n /10 ;
+        }
+        
+        if (three==1 && nine==1)
+        {
+            System.out.printf("Lucky number");
+        }
+        else
+        {
+            System.out.println("NOT a lucky number");    
+        }
+    }
+}
