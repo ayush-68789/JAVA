f466a3c86fbec8f1e440008b8139581fc0abed70
Ayush chaurasia <ayush.chaurasia_cs24@gla.ac.in>
2025-07-04 18:00:51 +0530

    Update String Functions 2 .java

--- Strings/String Functions 2 .java
+++ Strings/String Functions 2 .java
@@ -5,10 +5,12 @@ class Main
     {
         Scanner sc = new Scanner(System.in) ;
         String str = sc.nextLine() ;
+        // str.append("P") ;                                    // insert the element at last positon
         //System.out.println(str.startsWith("Ay")) ;            // return true when string starts with given words
         //System.out.print(str.endsWith("sh")) ;                // return true when string ends with given wwords
         //System.out.print(str.charAt(4)) ;                     // returns the character at given index
         //System.out.println(str.setcharAt(3, 'P') ;            // at index 3 set char P 
+        // str.delete(start index , end index ) ;               // it goes to delete the str with starting index excluding the last index
         //        str.insert(0, 'T') ;                          // Insert T at first Index eg - Ony so at 0 we insert T and becomes Tony
         //System.out.print(str.indexOf("s")) ;                  // return the index of the given character
         //System.out.print(str.indexOf("g",3)) ;                // return the index after the given index when not present prints -1
