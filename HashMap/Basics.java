23e6ccee964dfcc358f2cf9fcfb7bbdd88d3b6b3
Ayush chaurasia <ayush.chaurasia_cs24@gla.ac.in>
2025-07-06 11:41:41 +0530

    Create Basics.java

---
+++ HashMap/Basics.java
@@ -0,0 +1,7 @@
+to use HashMAp       -->       import java.util.HashMap ;               or simply use this -->  imnport java.util.* ;
+
+to make a new hashmap     ==>    HashMap <String , Integer> var_name = new HashMap<>() ;
+
+to insert a key Value pair        -->          var_name.put(key , value) ;
+
+to 
