0a04d807658f35482009fcfdb9a04a1ff316f9e3
Ayush chaurasia <ayush.chaurasia_cs24@gla.ac.in>
2025-07-06 12:48:22 +0530

    Update Basics.java

--- HashMap/Basics.java
+++ HashMap/Basics.java
@@ -1,7 +1,32 @@
-to use HashMAp       -->       import java.util.HashMap ;               or simply use this -->  imnport java.util.* ;
+1. )   to use HashMAp       -->       import java.util.HashMap ;               or simply use this -->  imnport java.util.* ;
 
-to make a new hashmap     ==>    HashMap <String , Integer> var_name = new HashMap<>() ;
+2. )   to make a new hashmap     ==>    HashMap <String , Integer> var_name = new HashMap<>() ;
 
-to insert a key Value pair        -->          var_name.put(key , value) ;
+3. )   to insert a key Value pair        -->          var_name.put(key , value) ;
+                
+                public static void main(String[] args) 
+	              {
+		                  HashMap <String, Integer> map = new HashMap <>() ;
+		                  map.put("Piyush", 0) ;
+		                  map.put("ayush",1) ;
+		                  System.out.println(map);
+	              }
 
-to 
+o/p : {ayush=1, Piyush=0}        // wits means they are unordered 
+
+
+4. )   to search a key     -->      var_name.containsKey(name_of_key) ;               returns true if contains else returns false
+
+                if(map.containsKey("ayush"))
+		            {
+		                  System.out.print("Present");   
+		            }
+
+5. )   to get the value of the specific key      -->       var_name.get(name_of_key) ;       
+
+                System.out.print(map.get("Piyush"));     o/p - 0 
+                System.out.print(map.get("aksh"));       o/p - null
+
+
+6. )   Iteration in HashMaps : 
+		            
