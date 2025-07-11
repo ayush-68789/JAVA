73fa5d0243ccd6879617cdda9ee049537f39f403
Ayush chaurasia <ayush.chaurasia_cs24@gla.ac.in>
2025-07-11 16:58:18 +0530

    Update String Functions 1 .java

--- Strings/String Functions 1 .java
+++ Strings/String Functions 1 .java
@@ -1,14 +1,21 @@
-import java.util.Scanner ;
-class Main 
-{
-    public static void main(String args[])
-    {
-        Scanner sc = new Scanner(System.in) ;
-        String name = sc.nextLine() ;
-        //System.out.println(name.toLowerCase()) ;      //print to lowercase
-        //System.out.println(name.toUpperCase()) ;      // print to uppercase
-        //System.out.println(name.trim()) ;             // print string after removing trailing and ending spaces
-        //System.out.println(name.substring(4));        // return the substring starting with give index to the end of string
-        //System.out.println(name.substring(3,8));      // return the substring with given starting and ending index o string
-    }
-}
+Method                                                 Description                                            Example
+==================================================================================================================================================
+length()                                Returns the length of the string.                            "hello".length() -> 5
+
+charAt(int index)                    Returns the character at the specified index.                  "Java".charAt(1) -> 'a'
+
+concat(String str)          Concatenates the specified string to the end of the current string.      "Hello ".concat("World") -> "Hello World"
+
+substring(int beginIndex)        Returns a new string that is a substring of this string.            "example".substring(3) -> "mple"
+
+substring(int begin, int end)    Returns a substring starting at begin (inclusive) and ending at end (exclusive).      "example".substring(1, 4) -> "xam"
+
+indexOf(String str)            Returns the index of the first occurrence of the specified substring.            "apple".indexOf("pl") -> 2
+
+toLowerCase()                            Converts all characters to lowercase.                            "Hello".toLowerCase() -> "hello"
+
+toUpperCase()                            Converts all characters to uppercase                              "Hello".toUpperCase() -> "HELLO"
+
+trim()                    Returns a copy of the string, with leading and trailing whitespace removed.            "   test   ".trim() -> "test"
+
+split(String regex)            Splits the string around matches of the given regular expression.            "a,b,c".split(",") -> ["a", "b", "c"]
